b8ffd69450d3a9f588e94186ae2584323f52a490
lcdr <lcdr@users.noreply.github.com>
2015-08-29 15:55:00 +0200

    Made attributes private and changed object to obj.

--- replicamanager.py
+++ replicamanager.py
@@ -6,9 +6,9 @@ from .msgIDs import MsgID
 
 class ReplicaManager:
 	def __init__(self):
-		self.participants = set()
-		self.network_ids = {}
-		self.objects = {}
+		self._participants = set()
+		self._network_ids = {}
+		self._objects = {}
 		self._current_network_id = 0
 		asyncio.async(self._serialize_loop())
 		self.register_for_raknet_packet(MsgID.ReplicaManagerConstruction, self.on_construction)
@@ -16,81 +16,71 @@ class ReplicaManager:
 		self.register_for_raknet_packet(MsgID.ReplicaManagerSerialize, self.on_serialize)
 
 	def add_participant(self, address):
-		self.participants.add(address)
-		for obj in self.network_ids:
+		self._participants.add(address)
+		for obj in self._network_ids:
 			self.construct(obj, (address,), new=False)
 
 	def on_disconnect_or_connection_lost(self, data, address):
-		self.participants.discard(address)
+		self._participants.discard(address)
 
-	def construct(self, object, recipients=None, new=True):
+	def construct(self, obj, recipients=None, new=True):
 		# recipients is needed to send replicas to new participants
 		if recipients is None:
-			recipients = self.participants
+			recipients = self._participants
 
 		if new:
-			self.network_ids[object] = self._current_network_id
+			self._network_ids[obj] = self._current_network_id
 			self._current_network_id += 1
 
 		out = BitStream()
 		out.write(c_ubyte(MsgID.ReplicaManagerConstruction))
 		out.write(c_bit(True))
-		out.write(c_ushort(self.network_ids[object]))
-		out.write(object.send_construction())
+		out.write(c_ushort(self._network_ids[obj]))
+		out.write(obj.send_construction())
 
 		for recipient in recipients:
 			self.send(out, recipient)
 
-	def serialize(self, object):
+	def serialize(self, obj):
 		out = BitStream()
 		out.write(c_ubyte(MsgID.ReplicaManagerSerialize))
-		out.write(c_ushort(self.network_ids[object]))
-		out.write(object.serialize())
+		out.write(c_ushort(self._network_ids[obj]))
+		out.write(obj.serialize())
 
-		for participant in self.participants:
+		for participant in self._participants:
 			self.send(out, participant)
 
-	def destruct(self, object):
-		print("destructing", object)
+	def destruct(self, obj):
+		print("destructing", obj)
 		out = BitStream()
 		out.write(c_ubyte(MsgID.ReplicaManagerDestruction))
-		out.write(c_ushort(self.network_ids[object]))
+		out.write(c_ushort(self._network_ids[obj]))
 
-		for participant in self.participants:
+		for participant in self._participants:
 			self.send(out, participant)
 
-		del self.network_ids[object]
+		del self._network_ids[obj]
 
 	def _serialize_loop(self):
 		while True:
-			for obj in self.network_ids:
+			for obj in self._network_ids:
 				if obj._serialize:
 					self.serialize(obj)
 					obj._serialize = False
 			yield from asyncio.sleep(0.3)
 
 	def on_construction(self, construction, address):
-		if address in self.participants:
+		if address in self._participants:
 			assert construction.read(c_bit)
 			network_id = construction.read(c_ushort)
-			self.objects[network_id] = self.on_replica_manager_construction(construction)
+			self._objects[network_id] = self.on_replica_manager_construction(construction)
 
 	def on_serialize(self, serialize, address):
-		if address in self.participants:
+		if address in self._participants:
 			network_id = serialize.read(c_ushort)
-			self.objects[network_id].deserialize(serialize)
+			self._objects[network_id].deserialize(serialize)
 
 	def on_destruction(self, destruction, address):
-		if address in self.participants:
+		if address in self._participants:
 			network_id = destruction.read(c_ushort)
-			del self.objects[network_id]
-
-class Replica:
-	def __init__(self, manager):
-		print("Game object created")
-		self._manager = manager
-		self._serialize = False
-
-	def __del__(self):
-		print("Game object destroyed")
-		self._manager.destruct(self)
+			del self._objects[network_id]
